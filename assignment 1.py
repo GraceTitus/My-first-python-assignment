cf8595610ef7e6ffa528eae342ea09d30dbf7f06
GraceTitus <gracendungu10@gmail.com>
2024-02-27 16:52:23 +0300

    Initial commit

---
+++ assignment 1.py
@@ -0,0 +1,29 @@
+print("hello there!")
+a, b, c = 5, 10, 15
+
+print(a) # prints 5
+print(b) # prints 10
+print(c) # prints 15
+
+a=b=c=5
+
+print(a)
+print(b)
+print(c)
+
+languages= ("swift", "java", "python")
+
+print(languages)
+print (languages[0])
+print (languages[2])
+print(languages[1])
+
+#start from 100 to 200 to 300
+student_id = {100, 200, 300}
+
+print(student_id)
+print(type(student_id))
+
+
+capital_city = {'kenya':'nairobi', 'uganda':'kampala', 'Rwanda':'kigali'}
+print(capital_city)
---
+++ user_input.py
@@ -0,0 +1,15 @@
+name = input("tell me your name:")
+
+print(name)
+
+age = input("tell me your age")
+
+print(age)
+
+location = input("tell me your location:")
+print(location)
+
+print(f"Hello {name}, you are {age} years old and live in {location}.")
+
+
+

a6ac730a6b487d935ed851e8d2524254f77cbf87
GraceTitus <gracendungu10@gmail.com>
2024-02-28 16:15:54 +0300

    I love python

---
+++ PLP_data types/data_types_lessons.py
@@ -0,0 +1,41 @@
+a=100
+b=3.14
+#print(a*b)
+c=5+7j
+#print(a+b-c)
+d=False
+#print(d)
+#print(a+d)
+#print(a/b)
+str="Welcome to plp Academy"
+#print(str)
+#print(str[0])
+#print(str[-1])
+#print(str[4:10])
+plp="python class"
+#print(str + plp)
+#print(plp * 4)
+Shopping_list =['braed','milk',20, 40.5, 5+8j]
+#print(Shopping_list)
+Building_list = [450,'wheelbarros', 180, 'shovels','bread', 'milk']
+#print(Building_list)
+#print(Shopping_list[0])
+#print(Shopping_list[4:5])
+#print(Shopping_list + Building_list)
+#print(Shopping_list * 2 )
+plp_tuple = 'abcd',786, 2.23, "john", 70.2
+#print(plp_tuple)
+#print (plp_tuple)
+tiny_tuple = ("John", 5+7j)
+#print(tiny_tuple)
+#print(plp_tuple + tiny_tuple)
+#print(tiny_tuple [1])
+dict = {}
+dict['one'] ='this is one'
+dict['two']= 'this is two'
+tiny_dict ={'name': 'John','code':6734, 'dept': 1007}
+print(dict['one'])
+print(dict['two'])
+print(tiny_dict)
+print(tiny_dict.keys())
+print(tiny_dict.values())
